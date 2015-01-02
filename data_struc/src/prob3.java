00806cca638d2a1de137f32fb6e59f49b2eb7fac
aayushuppal <aayushup@buffalo.edu>
2015-01-02 04:07:44 -0500

    intial implementation done
    considers whitespaces

--- data_struc/src/prob3.java
+++ data_struc/src/prob3.java
@@ -1,11 +1,13 @@
 /* Given two strings, write a method to decide if one is a permutation of the other
 Aayush Uppal - http://uppalaayush.wordpress.com/profile */
 import java.util.*;
+import java.util.Arrays;
 
 public class prob3{
 	
 	public static void main(String[] args){
 	prob3 pobj = new prob3();
+	System.out.println("Check if a permuation:");
 	Scanner inp = new Scanner(System.in);	
 	
 	String St1 = inp.nextLine();
@@ -18,7 +20,21 @@ public class prob3{
 	
 	public boolean CheckPerm(String a, String b){
 	boolean otpt = false;	
-	return otpt;	
+	
+		if (a.length() != b.length()){
+			return otpt;
+		}
+		else{
+			char[] x = a.toCharArray();
+			char[] y = b.toCharArray();
+			
+			Arrays.sort(x);
+			Arrays.sort(y);
+			if (Arrays.equals(x, y)){
+				otpt = true;
+			}
+			return otpt;	
+		}	
 	}
 	
 	
