c765050641105c91403a26b032c44c12ef3116c2
aayushuppal <aayushup@buffalo.edu>
2015-01-02 03:49:50 -0500

    updated method structure

--- data_struc/src/prob3.java
+++ data_struc/src/prob3.java
@@ -1,11 +1,24 @@
 /* Given two strings, write a method to decide if one is a permutation of the other
 Aayush Uppal - http://uppalaayush.wordpress.com/profile */
+import java.util.*;
 
 public class prob3{
 	
 	public static void main(String[] args){
-		
-		
+	prob3 pobj = new prob3();
+	Scanner inp = new Scanner(System.in);	
+	
+	String St1 = inp.nextLine();
+	String St2 = inp.nextLine();
+	
+	inp.close();
+	System.out.println(pobj.CheckPerm(St1,St2));
+	
+	}
+	
+	public boolean CheckPerm(String a, String b){
+	boolean otpt = false;	
+	return otpt;	
 	}
 	
 	
