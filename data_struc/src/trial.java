3854f460247e720a48f2b5c4dda7c61c6f733f04
aayushuppal <aayushup@buffalo.edu>
2015-01-02 00:39:32 -0500

    initial commit

---
+++ data_struc/src/trial.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+
+public class trial {
+	public static void main(String[] args) {
+		TreeMap<Integer, String> asd = new TreeMap<Integer, String>();
+		asd.put(12, "asasaas");
+		System.out.println(asd.get(12));
+	
+		HashMap<Integer, String> fgh = new HashMap<Integer,String>();
+		fgh.put(1, "asasas");
+		System.out.println(fgh.get(1));
+		
+		ArrayList<Integer> narr = new ArrayList<Integer>();
+		narr.add(12);
+		System.out.println(narr.get(0));
+		
+	}
+
+}	
